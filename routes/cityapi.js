1ce2104ef67f2854f0a1745ce121ee2a0ef4f2d4
willjthorp <willthorp99@gmail.com>
2017-10-03 20:43:35 +0200

    sorted google places api call

--- routes/cityapi.js
+++ routes/cityapi.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var parseString = require('xml2js').parseString;
 
 const https = require('https');
+const http = require('http');
+
+
 
 router.get('/indices/:city', (req, res, next) => {
-  console.log('hello');
   https.get(`https://www.numbeo.com/api/indices?api_key=ghhbagkcagbicb&query=` + req.params.city, (resp) => {
     let data = '';
 
@@ -15,7 +18,6 @@ router.get('/indices/:city', (req, res, next) => {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-      console.log(data);
       res.json(JSON.parse(data));
     });
 
@@ -23,5 +25,38 @@ router.get('/indices/:city', (req, res, next) => {
     res.json("Error: " + err.message);
   });
 });
+
+
+
+router.get('/photoreference/:city', (req, res, next) => {
+  https.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.params.city}&key=AIzaSyCRBJhxq4B8uCjH3NTkCWO34M6UVQO-Sfg`, (resp) => {
+    let data = '';
+    let jsonData;
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      res.json(JSON.parse(data));
+    });
+
+  }).on("error", (err) => {
+    res.json("Error: " + err.message);
+  });
+});
+
+
+router.get('/photo/:photo', (req, res, next) => {
+  https.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=2000&photoreference=${req.params.photo}&key=AIzaSyCRBJhxq4B8uCjH3NTkCWO34M6UVQO-Sfg`, (resp) => {
+    console.log('resp:', resp.headers.location);
+    res.json({location: resp.headers.location});
+  }).on("error", (err) => {
+    res.json("Error: " + err.message);
+  });
+});
+
 
 module.exports = router;
