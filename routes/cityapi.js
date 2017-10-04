3f18faccb2532c523115c1e29009ee5db87cfcaf
willjthorp <willthorp99@gmail.com>
2017-10-04 19:53:48 +0200

    added climate api call

--- routes/cityapi.js
+++ routes/cityapi.js
@@ -6,49 +6,43 @@ const https = require('https');
 const http = require('http');
 
 
-
+// Get indices information for city
 router.get('/indices/:city', (req, res, next) => {
-  https.get(`https://www.numbeo.com/api/indices?api_key=ghhbagkcagbicb&query=` + req.params.city, (resp) => {
+  https.get(`https://www.numbeo.com/api/indices?api_key=ghhbagkcagbicb&query=${req.params.city}`, (resp) => {
     let data = '';
-
     // A chunk of data has been recieved.
     resp.on('data', (chunk) => {
       data += chunk;
     });
-
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
       res.json(JSON.parse(data));
     });
-
   }).on("error", (err) => {
     res.json("Error: " + err.message);
   });
 });
 
 
-
+// Get google photo reference for city
 router.get('/photoreference/:city', (req, res, next) => {
   https.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.params.city}&key=AIzaSyCRBJhxq4B8uCjH3NTkCWO34M6UVQO-Sfg`, (resp) => {
     let data = '';
-    let jsonData;
-
     // A chunk of data has been recieved.
     resp.on('data', (chunk) => {
       data += chunk;
     });
-
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
       res.json(JSON.parse(data));
     });
-
   }).on("error", (err) => {
     res.json("Error: " + err.message);
   });
 });
 
 
+// Use photo reference to retrieve city photo
 router.get('/photo/:photo', (req, res, next) => {
   https.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=2000&photoreference=${req.params.photo}&key=AIzaSyCRBJhxq4B8uCjH3NTkCWO34M6UVQO-Sfg`, (resp) => {
     console.log('resp:', resp.headers.location);
@@ -57,6 +51,44 @@ router.get('/photo/:photo', (req, res, next) => {
     res.json("Error: " + err.message);
   });
 });
+
+
+// Get city prices information
+router.get('/prices/:city', (req, res, next) => {
+  https.get(`https://www.numbeo.com/api/city_prices?api_key=ghhbagkcagbicb&query=${req.params.city}`, (resp) => {
+    let data = '';
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      res.json(JSON.parse(data));
+    });
+  }).on("error", (err) => {
+    res.json("Error: " + err.message);
+  });
+});
+
+
+// Get city climate information
+router.get('/city_climate/:city', (req, res, next) => {
+  https.get(`https://www.numbeo.com/api/city_climate?api_key=ghhbagkcagbicb&query=${req.params.city}`, (resp) => {
+    let data = '';
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      res.json(JSON.parse(data));
+    });
+  }).on("error", (err) => {
+    res.json("Error: " + err.message);
+  });
+});
+
+
 
 
 module.exports = router;
